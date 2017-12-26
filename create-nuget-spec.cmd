396b3ee51cac16a0bd5a7e3ef4f29fabd4568c31
Jeferson Tenorio <jeferson.tenorio@ctis.com.br>
2017-12-26 15:48:37 -0200

    added icon

--- create-nuget-spec.cmd
+++ create-nuget-spec.cmd
@@ -1,1 +1,1 @@
-.\packages\NuspecFromGithub.1.0.5\bin\Debug\NuspecFromGithub.exe -p .\src\m3uParser.tool --force -g jefersonsv/m3uParser
+.\packages\NuspecFromGithub.1.0.5\bin\Debug\NuspecFromGithub.exe -p .\src\m3uParser --force -g jefersonsv/m3uParser
